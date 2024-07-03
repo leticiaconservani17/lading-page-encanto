8a65969bca76c86ba33d5c9a14440cf880109713
leticiaconservani17 <leticiaconservani@gmail.com>
2024-07-03 15:24:57 -0300

    Subindo segunda seção e aplicando responsividade

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('ffmpeg-static');
+
+// Define o caminho do executável ffmpeg
+ffmpeg.setFfmpegPath(ffmpegPath);
+
 module.exports = function(grunt) {
+
+    // Adicione todas as definições e chamadas do 'grunt' dentro desta função
+    function getVideoFiles(srcPath) {
+        return fs.readdirSync(srcPath).filter(file => {
+            return file.endsWith('.mp4'); 
+        }).map(file => {
+            return path.join(srcPath, file);
+        });
+    }
+
+    function compileVideos(done) {
+        const videoFiles = getVideoFiles('src/videos');
+        const outputDir = 'build/videos';
+
+        // Cria o diretório de saída se ele não existir
+        if (!fs.existsSync(outputDir)) {
+            fs.mkdirSync(outputDir, { recursive: true });
+        }
+
+        const tasks = videoFiles.map(file => {
+            const outputFile = path.join(outputDir, path.basename(file));
+            return new Promise((resolve, reject) => {
+                ffmpeg(file)
+                    .outputOptions('-vf', 'scale=1280:720')
+                    .on('end', () => resolve())
+                    .on('error', err => reject(err))
+                    .save(outputFile);
+            });
+        });
+
+        Promise.all(tasks)
+            .then(() => done())
+            .catch(err => {
+                grunt.log.error(err);
+                done(false);
+            });
+    }
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         sass: {
@@ -28,11 +74,11 @@ module.exports = function(grunt) {
             }
         },
         imagemin: {
-            dynamic: {
+            dist: {
                 files: [{
                     expand: true,
                     cwd: 'src/images/',
-                    src: ['**/*.{png,jpg,gif,svg}'],
+                    src: ['**/*.{png,jpg,gif}'],
                     dest: 'build/images/'
                 }]
             }
@@ -89,19 +135,36 @@ module.exports = function(grunt) {
         watch: {
             sass: {
                 files: ['src/styles/**/*.scss'],
-                tasks: ['sass']
+                tasks: ['sass'],
+                options: {
+                    livereload: true,
+                }
             },
             imagemin: {
-                files: ['src/images/**/*'],
-                tasks: ['imagemin']
+                files: ['src/images/**/*.{png,jpg,gif}'],
+                tasks: ['imagemin:dist'],
+                options: {
+                    spawn: false,
+                    livereload: true,
+                }
             },
             html: {
                 files: ['src/index.html'],
-                tasks: ['htmlmin', 'replace']
+                tasks: ['htmlmin', 'replace'],
+                options: {
+                    livereload: true,
+                },
+            },
+            videos: {
+                files: ['src/videos/*'],
+                tasks: ['compileVideos'],
+                options: {
+                    livereload: true,
+                },
             }
         },
         concurrent: {
-            target: ['sass', 'imagemin', 'htmlmin', 'replace']
+            target: ['sass', 'imagemin:dist', 'htmlmin', 'replace', 'compileVideos']
         }
     });
 
@@ -112,6 +175,13 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
+    // Tarefa personalizada para compilar vídeos
+    grunt.registerTask('compileVideos', 'Compile video files', function() {
+        const done = this.async();
+        compileVideos(done);
+    });
+
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['concurrent']);
+    grunt.registerTask('build', ['concurrent:target']);
 };
+
